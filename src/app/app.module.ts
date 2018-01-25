ce6e6015944d1f33c69b15b5ccc4babc9d22f409
subzerodeluxe <maarten.waanders@gmail.com>
2018-01-25 14:24:33 +0100

    Added dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { DistanceWords } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+// Native
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { Facebook } from "@ionic-native/facebook";
+import { Camera } from '@ionic-native/camera';
+import { Network } from '@ionic-native/network';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @NgModule({
   declarations: [
-    MyApp,
+    DistanceWords,
     HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(DistanceWords)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    DistanceWords,
     HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
+    Network,
+    Geolocation,
+    Facebook,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
