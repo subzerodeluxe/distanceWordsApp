09267d79a24fa1f5ede40687821cb3c6331a7c43
subzerodeluxe <maarten.waanders@gmail.com>
2018-01-25 15:46:31 +0100

    Added gitignore file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, MenuController, ModalController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { DashboardPage } from '../pages/dashboard/dashboard';
 
-import { HomePage } from '../pages/home/home';
+import * as firebase from 'firebase/app'; 
+import { UserServiceProvider } from '../providers/user-service/user-service';
+import { AlertServiceProvider } from '../providers/alert-service/alert-service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+import { NavController } from 'ionic-angular/navigation/nav-controller';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class DistanceWords {
-  rootPage:any = HomePage;
+  public firebase : any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, public user: UserServiceProvider, splashScreen: SplashScreen, public alerts: AlertServiceProvider, 
+    public menuCtrl: MenuController, public navCtrl: NavController, public modalCtrl: ModalController, public afAuth: AngularFireAuth, public auth: AuthServiceProvider) {
+      
+      this.firebase = firebase; 
+      firebase.auth().onAuthStateChanged(user => {
+        if(user) { // when user is authenticated 
+          this.navCtrl.setRoot('credentials');
+        } else {
+          this.navCtrl.setRoot('onboarding'); 
+        }
+      });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { DistanceWords } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 // Native
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,19 +12,54 @@ import { Camera } from '@ionic-native/camera';
 import { Network } from '@ionic-native/network';
 import { Geolocation } from '@ionic-native/geolocation';
 
+// Providers
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+import { UserServiceProvider } from '../providers/user-service/user-service';
+import { AlertServiceProvider } from '../providers/alert-service/alert-service';
+
+// Pages
+import { DashboardPage } from '../pages/dashboard/dashboard';
+import { LoginPage } from '../pages/login/login';
+import { OnboardingPage } from '../pages/onboarding/onboarding';
+import { CheckCredentialsPage } from '../pages/check-credentials/check-credentials';
+
+// AF2 Modules
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// setup Firebase credentials
+export const firebaseConfig = {
+  apiKey: "AIzaSyC5t0aZExyge2QPEbsQRI18cQJopL5QHns",
+  authDomain: "distancewords-29ab5.firebaseapp.com",
+  databaseURL: "https://distancewords-29ab5.firebaseio.com",
+  projectId: "distancewords-29ab5",
+  storageBucket: "distancewords-29ab5.appspot.com",
+  messagingSenderId: "356270271131"
+};
+
 @NgModule({
   declarations: [
     DistanceWords,
-    HomePage
+    DashboardPage,
+    LoginPage,
+    OnboardingPage,
+    CheckCredentialsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(DistanceWords)
+    IonicModule.forRoot(DistanceWords),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     DistanceWords,
-    HomePage
+    DashboardPage,
+    LoginPage,
+    OnboardingPage,
+    CheckCredentialsPage
   ],
   providers: [
     StatusBar,
@@ -34,7 +68,10 @@ import { Geolocation } from '@ionic-native/geolocation';
     Network,
     Geolocation,
     Facebook,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthServiceProvider,
+    UserServiceProvider,
+    AlertServiceProvider
   ]
 })
 export class AppModule {}
