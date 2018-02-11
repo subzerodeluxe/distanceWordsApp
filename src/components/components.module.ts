93fccff868a26c13277abd6c2b2718bb09b7ab97
subzerodeluxe <maarten.waanders@gmail.com>
2018-02-11 21:57:05 +0100

    Refactoring modules

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -8,7 +8,8 @@ import { SmileRateComponent } from './smile-rate/smile-rate';
     MoodModalComponent,
     SmileRateComponent],
 	imports: [],
-	exports: [CountryComponent,
+	exports: [
+	CountryComponent,
     MoodModalComponent,
     SmileRateComponent]
 })
